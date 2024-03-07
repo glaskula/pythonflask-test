51d60ed546525625a6e27873c5ea62ba8f8e63d4
Victorcousin <Victorcou123@gmail.com>
2024-03-07 16:45:24 +0100

    gunicorn add

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-
-
+ 
 import os
 
 from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
@@ -18,8 +17,3 @@ async def ask():
     chatask = await askQuestion(q,llm, rds, PROMPT_SV, PROMPT_EN, memory_Rephrase, memory,QA_CHAIN_PROMPT_SV, QA_CHAIN_PROMPT_EN)
     return chatask['result']
 
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
