b6c266a36af3ce2a6bc810b119f54cefd0c28356
Victorcousin <Victorcou123@gmail.com>
2024-03-07 14:17:59 +0100

    test askQuestion

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, request
 
 import os
 
-#from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
-#from Chat import askQuestion
+from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
 
 app = Flask(__name__)
 
@@ -16,8 +15,8 @@ def index():
 def ask():
     q = request.form.get('question')
     print(q)
-    #chatask = askQuestion(q)
-    return q
+    chatask = askQuestion(q,llm, rds, PROMPT_SV, PROMPT_EN, memory_Rephrase, memory,QA_CHAIN_PROMPT_SV, QA_CHAIN_PROMPT_EN)
+    return chatask
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
