1881e7ea259e839a00ed0b5307fe4842a60f9794
glaskula <philip.rabia@hotmail.com>
2024-03-21 14:43:19 +0100

    Opened it to everything so we can test locally.

--- application.py
+++ application.py
@@ -6,7 +6,8 @@ import os
 from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
 app = Flask(__name__)
 
-CORS(app, resources={r"/ask": {"origins": "https://react-chat-bot-002ep-ai-xjob.apps.ocpext.gbgpaas.se"}})
+CORS(app)
+#CORS(app, resources={r"/ask": {"origins": "https://react-chat-bot-002ep-ai-xjob.apps.ocpext.gbgpaas.se"}})
 
 
 @app.route('/')
