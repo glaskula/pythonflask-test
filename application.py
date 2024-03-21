3f5930dbeb3ab725aa37ab99c4d85dabc32cb67c
glaskula <philip.rabia@hotmail.com>
2024-03-21 14:19:39 +0100

    Moved position of app

--- application.py
+++ application.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 import os
 
 from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
+app = Flask(__name__)
 
 CORS(app, resources={r"/ask": {"origins": "https://react-chat-bot-002ep-ai-xjob.apps.ocpext.gbgpaas.se"}})
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
