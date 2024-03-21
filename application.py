e732f6867dd395e1880453e0f1eb8b6520eb397b
Victorcousin <Victorcou123@gmail.com>
2024-03-21 13:34:27 +0100

    Revert "testing React"
    This reverts commit f35380eba8a3ad3cd307967b5a9d168c352439cd.

--- application.py
+++ application.py
@@ -1,18 +1,19 @@
-from flask import Flask, request, jsonify
-from Chat import llm, rds, PROMPT_EN, PROMPT_SV, memory_Rephrase, memory, QA_CHAIN_PROMPT_SV, QA_CHAIN_PROMPT_EN, askQuestion
+from flask import Flask, render_template, request
+ 
+import os
+
+from Chat import redis_url, index_name, schema_name, inference_server_url, embeddings, rds, llm, PROMPT_EN, QA_CHAIN_PROMPT_EN, PROMPT_SV, QA_CHAIN_PROMPT_SV, memory_Rephrase, memory, askQuestion
 
 app = Flask(__name__)
 
-# Route for handling chat requests
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.json
-    question = data.get('question')
-    if not question:
-        return jsonify({'error': 'Missing question parameter'}), 400
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    chat_response = askQuestion(question, llm, rds, PROMPT_SV, PROMPT_EN, memory_Rephrase, memory, QA_CHAIN_PROMPT_SV, QA_CHAIN_PROMPT_EN)
-    return jsonify(chat_response)
+@app.route('/ask', methods=['POST'])
+async def ask():
+    q = request.form.get('question')
+    print(q)
+    chatask = await askQuestion(q,llm, rds, PROMPT_SV, PROMPT_EN, memory_Rephrase, memory,QA_CHAIN_PROMPT_SV, QA_CHAIN_PROMPT_EN)
+    return chatask['result']
 
-if __name__ == '__main__':
-    app.run(debug=True)
